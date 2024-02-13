4be017c8d29c8c1c998ca4dc9a0b3d65a1bccfde
Artem Vysotsky <artem.vysotsky@gmail.com>
2024-02-13 17:42:35 -0500

    make it mobile friendly

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -97,7 +97,7 @@ export default async function RootLayout({
             resources={resources}
           >
             <Toaster richColors position="top-center" duration={3000} />
-            <div className="bg-background text-foreground flex h-screen flex-col items-center">
+            <div className="bg-background text-foreground flex h-full flex-col items-center">
               {session ? <GlobalState>{children}</GlobalState> : children}
             </div>
           </TranslationsProvider>
