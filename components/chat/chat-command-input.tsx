8d786dcc1f3364d261d1842a8e53940c51724e79
Artem Vysotsky <artem.vysotsky@gmail.com>
2024-03-18 13:12:07 -0400

    fix paywall

--- components/chat/chat-command-input.tsx
+++ components/chat/chat-command-input.tsx
@@ -47,7 +47,7 @@ export const ChatCommandInput: FC<ChatCommandInputProps> = ({}) => {
       >
         <PromptPicker />
 
-        <MessageHistoryPicker />
+        {/*<MessageHistoryPicker />*/}
 
         <FilePicker
           isOpen={isFilePickerOpen}
