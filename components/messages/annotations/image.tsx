e264577ceb5ca6f1fde5b500884cc783a60522b8
Artem Vysotsky <artem.vysotsky@gmail.com>
2024-04-09 18:57:58 -0700

    fix image optimization and formatting

--- components/messages/annotations/image.tsx
+++ components/messages/annotations/image.tsx
@@ -7,14 +7,26 @@ export default function AnnotationImage({
   annotation: Annotation
 }) {
   const imageParams = annotation.imageGenerator__generateImage!
+
+  const scale = 0.5
+  let width = 0,
+    height = 0
+
+  console.log(imageParams.size)
+
+  if (imageParams.size) {
+    width = parseInt(imageParams.size?.split("x")[0]) * scale
+    height = parseInt(imageParams.size?.split("x")[1]) * scale
+  }
+
   return (
     <div className={"my-4 w-1/2 sm:w-1/3"}>
       <a href={imageParams.url!} target={"_blank"}>
         <Image
           src={imageParams.url!}
           alt={imageParams.prompt}
-          width={0}
-          height={0}
+          width={width}
+          height={height}
           style={{ width: "100%", height: "auto" }}
           className="rounded-md"
         />
--- types/platformTools.ts
+++ types/platformTools.ts
@@ -31,6 +31,7 @@ export interface PlatformTool {
 export interface ImageGeneratorResult {
   url: string
   prompt: string
+  size: "1024x1024" | "1792x1024" | "1024x1792"
 }
 export interface GetYoutubeCaptionsResult {
   subtitles: {
