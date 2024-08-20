5dd3eb38224863fcad339bbd7d7cb4769f35c363
Artem Vysotsky <artem.vysotsky@gmail.com>
2024-08-19 21:36:16 -0700

    fix layout for assistants page

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,4 +1,7 @@
 import "../(private)/globals.css"
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google"
 
 export const metadata = {
   title: "ChatLabs",
@@ -13,6 +16,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Analytics />
+          <SpeedInsights />
+          <GoogleAnalytics gaId="G-Y14R2TP0QH" />
+          <GoogleTagManager gtmId={"GTM-5SBXJ23Q"} />
+        </>
+      )}
     </html>
   )
 }
