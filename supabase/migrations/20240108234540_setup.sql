1218a36f286cc6048333b7be2a82e8011349395c
mattlawlis <89171568+mattlawlis@users.noreply.github.com>
2024-09-15 09:20:09 -0500

    Update 20240108234540_setup.sql

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,9 +50,9 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
---  project_url TEXT := 'https://ukrdhiywigevlawgvmbj.supabase.co';
+--  project_url TEXT := 'https://raafsylaihmmamcxldwm.supabase.co';
   project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJhYWZzeWxhaWhtbWFtY3hsZHdtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNjQwOTEwOCwiZXhwIjoyMDQxOTg1MTA4fQ.C9NGXiaEs5z6PQAO38X0XsOxl8PbXrX5YAE4-_FPX2k'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
