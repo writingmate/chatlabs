afc73b636f3263a3031fa755b42bcabb605c25a3
Artem Vysotsky <artem.vysotsky@gmail.com>
2024-10-23 18:12:55 -0700

    fix visibility

--- supabase/migrations/20240906000001_update_tools_for_applications.sql
+++ supabase/migrations/20240906000001_update_tools_for_applications.sql
@@ -1,3 +1,3 @@
 -- Add a new column to the tools table
 ALTER TABLE tools
-ADD COLUMN can_be_used_in_ BOOLEAN NOT NULL DEFAULT false;
+ADD COLUMN can_be_used_in_applications BOOLEAN NOT NULL DEFAULT false;
